a849d5bd164d22f1a317f5e4bcd097fb179c6279
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-03-06 22:49:06 -0500

    Defining structure for project thumbnails

--- react_based_portfolio/src/Projects.js
+++ react_based_portfolio/src/Projects.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React from "react";
+import Thumbnail from "./Thumbnail";
  
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <Thumbnail
+      link="https://employee-directory-v2.herokuapp.com/"
+      image=""
+      title="Employee Directory App"
+      category="Deployed Bootcamp Coursework"
+      />
+      <Thumbnail
+      link="https://github.com/hector-gm/passwordGenerator"
+      image=""
+      title="Password Generator"
+      category="Bootcamp Coursework GitHub Repo"
+      />
+      <Thumbnail
+      link="https://novelty.herokuapp.com/"
+      image=""
+      title="Group Project"
+      category="Deployed Bootcamp Coursework"
+      />
+      <Thumbnail
+      link="https://hector-workout-tracker.herokuapp.com/"
+      image=""
+      title="Mongo Atlas enabled Fitness Tracker"
+      category="Deployed Bootcamp Coursework"
+      />
     </div>
   )
 }
